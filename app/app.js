85b5b1f2e2b598bc4b2e99834c77b58d26f3c344
yashraj1309 <dudhatrayashraj@gmail.com>
2022-07-07 13:16:36 +0530

    App update

--- app/app.js
+++ app/app.js
@@ -1,14 +1,15 @@
 var app = angular.module('weatherApp', ['angular-loading-bar']);
-
 app.controller('weatherControl', function($http,$scope){
 	$scope.items = {};
 	$scope.assetName = '';
 	$scope.totalCount = 0;
 	$scope.searchWeather = function(){
 		var searchTerm = $scope.assetName;
-		$http.get('http://api.openweathermap.org/data/2.5/find?q='+searchTerm+'&type=like&sort=population&cnt=30&units=metric&APPID=73136fa514890c15bc4534e7b8a1c0c4').success(function(data){
-			$scope.items = data.list;
-			$scope.totalCount = data.count;
+		// $http.get('http://api.openweathermap.org/data/2.5/find?q='+searchTerm+'&type=like&sort=population&cnt=30&units=metric&APPID=73136fa514890c15bc4534e7b8a1c0c4').success(function(data){
+		$http.get('http://api.weatherapi.com/v1/current.json?key=7a331c40bfb44cf0bf842911220707&q='+searchTerm
+		).success(function(data){
+		$scope.items = data;
+		console.log(data);
 		});
 	};
 });
